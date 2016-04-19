530448c20d29811c71903b06020071aba1dba614
DungPham <phamducdung1996@gmail.com>
2016-04-19 22:16:40 +0700

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,22 +2,47 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum (arr)
+  s=0
+  arr.each{|a| s+=a}
+  s
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum (arr)
+  while (n!=0)
+    for i in (0...n)
+      for j in (i+1..n)
+        if (arr[i]>=arr[j])
+          t=arr[i]
+          arr[i]=arr[j]
+          arr[j]=t
+        end
+      end
+    end
+    return arr[n]+arr[n-1]    
+  end 
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?( arr, n)
+    count=0
+    for i in (0...arr.Size)
+      for j in (i+1..arr.Size)
+        if ( arr[i] + arr[j] == n )
+          count+=1
+        end
+      end
+    end
+    if ( count != 0 )
+      return true 
+    else
+      return false
+    end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello"
 end
 
 def starts_with_consonant? s
