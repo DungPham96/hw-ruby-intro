3f0aad6c148dd62b6e4537979b649f265eb01f79
DungPham <phamducdung1996@gmail.com>
2016-05-14 16:50:33 +0700

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,7 +1,6 @@
 # Part 1
 
 # Define a method sum(array) that takes an array of integers as an argument and returns the sum of its elements.
-# For an empty array it should return zero.
 def sum (arr)
     s=0
     arr.each{|a| s+=a}
@@ -9,7 +8,6 @@ def sum (arr)
 end
 
 # Define a method max_2_sum(array) which takes an array of integers as an argument and returns the sum of its two largest elements.
-# For an empty array it should return zero. For an array with just one element, it should return that element.
 def max_2_sum (arr)
     
     s=0
@@ -31,8 +29,6 @@ def max_2_sum (arr)
 end
 
 # Define a method sum_to_n?(array, n) that takes an array of integers and an additional integer, n,
-# as arguments and returns true if any two elements in the array of integers sum to n.
-# sum_to_n?([], n) should return false for any value of n, by definition.    
 def sum_to_n? (arr, n)
     
     count=false
@@ -55,7 +51,6 @@ def hello(name)
 end
 
 # Define a method starts_with_consonant?(s) that takes a string and returns true if it starts with a consonant and false otherwise.
-# (For our purposes, a consonant is any letter other than A, E, I, O, U.)
 def starts_with_consonant? (s)
     start = false
     if ((s =~ /\A(?=[^aeiou])(?=[a-z])/i) == 0)
@@ -80,8 +75,6 @@ end
 # Define a class BookInStock which represents a book with an ISBN number, isbn, and price of the book as a floating-point number, price, as attributes.
 class BookInStock
     
-    # as the first argument and price as second argument, and should raise ArgumentError (one of Ruby's built-in exception types)
-    # if the ISBN number is the empty string or if the price is less than or equal to zero. Include the proper getters and setters for these attributes.
     def initialize (isbn, price)
         raise ArgumentError, "Argument not valid!" unless
         !(!(price > 0) or !(isbn != ""))
@@ -92,8 +85,6 @@ class BookInStock
     attr_accessor :isbn
     attr_accessor :price
     
-    # Include a method price_as_string that returns the price of the book formatted with a leading dollar sign and two decimal places,
-    # that is, a price of 20 should format as "$20.00" and a price of 33.8 should format as "$33.80".
     def price_as_string ()
        return "$#{'%.2f' % @price}" 
     end
